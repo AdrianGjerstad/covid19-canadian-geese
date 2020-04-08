e07eb9000b121713a049356d884254e722cf7af9
Adrian Gjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2020-04-08 15:58:44 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         covid19-canadian-geese
+// @namespace    https://adriangjerstad.github.io/
+// @version      1
+// @description  Replace every mention of coronavirus with "Canadian Geese"
+// @author       Adrian Gjerstad
+// @match        http*://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
